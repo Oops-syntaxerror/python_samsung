49dae4a1cd5ca6beb73be9173a8cce35cd274806
YashaswiniNM <yashaswini7moo@gmail.com>
2024-12-12 11:08:16 +0530

    Day 4

---
+++ python_building_blocks/mult.py
@@ -0,0 +1,8 @@
+# command_line_1.py
+
+import sys
+
+sum_of_numbers = 0
+print('User given numbers are')
+for i in range(1, len(sys.argv)):
+    p
--- python_building_blocks/two_digit.py
+++ python_building_blocks/two_digit.py
@@ -1,11 +1,6 @@
 # Check if user given number is 2 digit number
 
 input_number = int(input('Enter a number:'))
-temp = input_number
 
-while(input_number != 0):
-    input_number = input_number % 10
-    input_number /= 10
-
-if input_number == temp:
-    print(input_number,'is a 2-Digit number')
+if ( input_number > 9 and input_number < 100 ):
+    print('{input_number} is a 2-Digit number.')
